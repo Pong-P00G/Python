ce7e9907a9f229f09a7eb66f9fd82a0cec59d326
Srijan <noob123srijan@gmail.com>
2024-01-17 20:59:41 +0530

    Trial-102

---
+++ Day 1/py2.py
@@ -0,0 +1,9 @@
+#2D List
+
+def food():
+    food = [starters , main_corse , Dessert]
+    return food
+
+starters = ["Kababas","Paneer Tikka","Chichen"]
+main_corse = ["Biryani","Pulao","Dal Roti"]
+Dessert=["sweets",'ice-cream',"Pastry"]
